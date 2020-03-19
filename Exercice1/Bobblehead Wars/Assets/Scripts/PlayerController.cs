ab5859f4b8e567e9633aec43ca542681340a4dd1
garnier <perso.garnier@gmail.Com>
2020-03-19 08:16:33 +0100

    sauvegarde 19/3

---
+++ Exercice1/Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 50.0f;
+    private CharacterController characterController;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+
+    }
+}
