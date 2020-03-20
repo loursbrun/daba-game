626b629078ee3f348ad6550c96760bd314fcf575
garnier <perso.garnier@gmail.Com>
2020-03-20 07:29:03 +0100

    Save 20/3

--- Exercice1/Bobblehead Wars/Assets/Scripts/PlayerController.cs
+++ Exercice1/Bobblehead Wars/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 50.0f;
     private CharacterController characterController;
 
+    public Rigidbody head;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +24,20 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         characterController.SimpleMove(moveDirection * moveSpeed);
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (moveDirection == Vector3.zero)
+        {
+            // TODO
+        }
+        else
+        {
+            head.AddForce(transform.right * 150, ForceMode.Acceleration);
+        }
 
     }
 }
